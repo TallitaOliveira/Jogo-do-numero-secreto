4ef8baebd69f86f1366ac3f14725c4fda10a6834
Tallita Oliveira <tallita.oliveirasantos@hotmail.com>
2025-01-29 23:00:53 +0000

    Alterando o limite do numero Secreto

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
